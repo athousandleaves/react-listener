4a4066beb281fef76cf51e50768435662a6a01a4
athousandleaves <superamare@gmail.com>
2018-09-21 08:42:31 -0500

    Add basic styles and dom elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="pusher">
+          <div className="ui vertical masthead center aligned segment">
+            <div>
+              <h1 className="ui huge header" id="header">
+                Listener
+              </h1>
+            </div>
+            <div className="ui text container">
+              <h4 id="h4">
+                Search for an artist to view their top tracks
+                <i className="angle down icon" />
+              </h4>
+            </div>
+            <div className="ui container raised">
+              <div className="ui icon input raised">
+                <input
+                  className="text"
+                  placeholder="Type artist name.."
+                  id="input"
+                  autocomplete="off"
+                  autofocus
+                />
+                <i className="search link icon" id="search" />
+              </div>
+            </div>
+          </div>
+          <div id="songlist"></div>
+        </div>
       </div>
     );
   }
